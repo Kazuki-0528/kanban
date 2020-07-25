47ba752eb196fbd37e7e8d6226cce0a6be95ed69
github raamenzurururu <reku9696@gmail.com>
2020-07-25 10:26:53 +0900

    five commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :list, only: %i(create new edit update destroy) do
-    resources :card, only: %i(create new show)
+    resources :card, except: %i(index)
   end
 end
